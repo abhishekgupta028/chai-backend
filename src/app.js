60dd1297fe67e48b55446f49c844efcbf6089b08
abhishekgupta028 <abhishekgupta6205691283@gmail.com>
2025-10-30 20:08:33 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -10,11 +10,18 @@ app.use(cors({
 }));
 
 app.use(express.json({limit: "16kb"}));  // for parsing application/json
-
 app.use(express.urlencoded({limit: "16kb", extended: true}));  // for parsing application/x-www-form-urlencoded
-
 app.use(express.static('public'));  // for serving static files
-
 app.use(cookieParser());  // for parsing cookies
+
+
+// routes import added here
+
+import userRouter from './routes/user.routes.js';
+
+// routes declaration added here
+
+app.use('/api/v1/users', userRouter);
+//https://localhost:8000/api/v1/users/register
 
 export { app };
