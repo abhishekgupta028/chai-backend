71baaa8375ddcfa6f2927240e215ad49065375ca
abhishekgupta028 <abhishekgupta6205691283@gmail.com>
2025-11-06 22:12:49 +0530

    all user controller bugs and one route.js bug fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,4 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
-import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
 import { User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
@@ -20,7 +18,7 @@ const registerUser = asyncHandler( async (req, res) => {
 
 
     const {fullName, email, username, password } = req.body
-    //console.log("email: ", email);
+   // console.log("email: ", email);
 
     if (
         [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -35,8 +33,7 @@ const registerUser = asyncHandler( async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
-    //console.log(req.files);
-
+    console.log("FILES RECEIVED:", req.files);
     const avatarLocalPath = req.files?.avatar[0]?.path;
     //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
